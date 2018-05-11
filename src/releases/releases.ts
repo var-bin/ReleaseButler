2ddd218c0d81a3d3fc0e7eb35e4b681aa8390405
var-bin <var-bin@users.noreply.github.com>
2018-05-11 22:49:36 +0300

    Use `puppeteer` for testing screenshots

--- src/releases/releases.ts
+++ src/releases/releases.ts
@@ -1,4 +1,6 @@
-const octokit = require("@octokit/rest")();
+import * as octokit_rest from "@octokit/rest";
+const octokit: octokit_rest = require("@octokit/rest")();
+import * as puppeteer from "puppeteer";
 
 import { FrameworksConfig } from "../config/frameworks.config";
 
@@ -10,7 +12,39 @@ export class ReleasesModule {
 
   constructor() {
     this.latestReleaseBody
-      .then(body => console.log(body));
+      .then(text => {
+        octokit.misc.renderMarkdown({
+          text
+        })
+          .then(renderValue => {
+            console.log("renderMarkdown: \n", renderValue.data);
+
+            (async () => {
+              const browser = await puppeteer.launch();
+              const page = await browser.newPage();
+              const id = Date.now();
+
+              await page.setContent(renderValue.data);
+              console.log(await page.mainFrame().content());
+              await page.screenshot({
+                path: `${id}.jpg`
+              });
+
+              await browser.close();
+            })();
+          });
+      });
+    /* this.latestReleaseBody
+      .then(text => {
+        (async () => {
+          const browser = await puppeteer.launch();
+          const page = await browser.newPage();
+          await page.goto('https://example.com');
+          await page.screenshot({ path: 'example.png' });
+
+          await browser.close();
+        })();
+      }); */
   }
 
   get latestRelease() {
