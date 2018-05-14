090601653def0e1082fb69b44949b5aa0caa1262
var-bin <var-bin@users.noreply.github.com>
2018-05-14 23:14:00 +0300

    Tests

--- src/releases/releases.ts
+++ src/releases/releases.ts
@@ -1,10 +1,13 @@
 import * as octokit_rest from "@octokit/rest";
 const octokit: octokit_rest = require("@octokit/rest")();
 import * as puppeteer from "puppeteer";
+import * as path from "path";
+import * as fs from "fs";
 
 import { FrameworksConfig } from "../config/frameworks.config";
 
 const frameworksConfig = new FrameworksConfig();
+const SCREENSHOT_PATH = path.resolve("src/assets/images");
 
 export class ReleasesModule {
   owner = frameworksConfig.getFrameworkOwner("react");
@@ -17,34 +20,33 @@ export class ReleasesModule {
           text
         })
           .then(renderValue => {
-            console.log("renderMarkdown: \n", renderValue.data);
+            // for testing reasons
+            /* console.log("renderMarkdown: \n", renderValue.data);
+            fs.writeFileSync("index.html", `<div class="release-holder">` + renderValue.data + `</div>`, { encoding: "utf-8"}); */
 
             (async () => {
               const browser = await puppeteer.launch();
               const page = await browser.newPage();
               const id = Date.now();
+              const pageContent = `<div class="release-holder">${renderValue.data}</div>`;
 
-              await page.setContent(renderValue.data);
+              await page.setContent(pageContent);
               console.log(await page.mainFrame().content());
+              await page.addStyleTag({
+                path: path.resolve("src/assets/css/material.min.css")
+              });
+              await page.addStyleTag({
+                path: path.resolve("src/assets/css/styles.css")
+              });
               await page.screenshot({
-                path: `${id}.jpg`
+                path: path.join(SCREENSHOT_PATH, `${id}.png`),
+                fullPage: true
               });
 
               await browser.close();
             })();
           });
       });
-    /* this.latestReleaseBody
-      .then(text => {
-        (async () => {
-          const browser = await puppeteer.launch();
-          const page = await browser.newPage();
-          await page.goto('https://example.com');
-          await page.screenshot({ path: 'example.png' });
-
-          await browser.close();
-        })();
-      }); */
   }
 
   get latestRelease() {
