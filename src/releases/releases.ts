d86e702167c7ff8e4794e0d1fea322c96c56d5a6
var-bin <var-bin@users.noreply.github.com>
2018-05-18 22:11:48 +0300

    Refactoring: Use `Screenshots` module for making screenshot

--- src/releases/releases.ts
+++ src/releases/releases.ts
@@ -1,13 +1,12 @@
 import * as octokit_rest from "@octokit/rest";
 const octokit: octokit_rest = require("@octokit/rest")();
-import * as puppeteer from "puppeteer";
-import * as path from "path";
-import * as fs from "fs";
 
 import { FrameworksConfig } from "../config/frameworks.config";
 
 const frameworksConfig = new FrameworksConfig();
-const SCREENSHOT_PATH = path.resolve("src/assets/images");
+
+import { Screenshots } from "../screenshots/screenshots";
+const screenshots = new Screenshots();
 
 export class ReleasesModule {
   owner = frameworksConfig.getFrameworkOwner("react");
@@ -19,35 +18,9 @@ export class ReleasesModule {
         octokit.misc.renderMarkdown({
           text: text.body
         })
-          .then(renderValue => {
-            // for testing reasons
-            /* console.log("renderMarkdown: \n", renderValue.data);
-            fs.writeFileSync("index.html", `<div class="release-holder">` + renderValue.data + `</div>`, { encoding: "utf-8"}); */
-
-            console.log(renderValue.data);
-
-            (async () => {
-              const browser = await puppeteer.launch();
-              const page = await browser.newPage();
-              const id = Date.now();
-              const pageContent = `<div class="release-holder">${renderValue.data}</div>`;
-
-              await page.setContent(pageContent);
-              console.log(await page.mainFrame().content());
-              await page.addStyleTag({
-                path: path.resolve("src/assets/css/material.min.css")
-              });
-              await page.addStyleTag({
-                path: path.resolve("src/assets/css/styles.css")
-              });
-              await page.screenshot({
-                path: path.join(SCREENSHOT_PATH, `${id}.png`),
-                fullPage: true
-              });
-
-              await browser.close();
-            })();
-          });
+        .then(renderValue => {
+          screenshots.makeScreenshot(renderValue.data);
+        });
       });
   }
 
